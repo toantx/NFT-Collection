2f0c88709ea1c1abc0b5c3fbb55b7e7a96669b55
Toan Xuan Ta <toantx@smartosc.com>
2023-04-25 11:47:22 +0700

    update metaurl

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xd6bE73C6B08436A109365dd8BC3001f08E4E11D7";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-two-pied.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-3jexc7m17-toantx.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
